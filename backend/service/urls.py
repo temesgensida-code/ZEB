e997788b136e3f6ec319493c29fd78d6e8bd979f
Temesgen Sida <temesgensids@gmail.com>
2026-05-15 22:31:15 +0300

    status endpoint define

--- backend/service/urls.py
+++ backend/service/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import UrlSafetyCheckView
+from .views import UrlSafetyCheckView, UrlCheckProgressView
 
 urlpatterns = [
     path('check-url/', UrlSafetyCheckView.as_view(), name='check-url'),
+    path('check-progress/', UrlCheckProgressView.as_view(), name='check-progress'),
 ]
